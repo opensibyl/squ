6fe99c8f6885844b7e63d751faca994fb1b8d4dd
williamfzc <178894043@qq.com>
2023-03-19 12:18:39 +0800

    fix(py): wrong command still ..

--- runner/runner_pytest.go
+++ runner/runner_pytest.go
@@ -13,18 +13,21 @@ type PytestRunner struct {
 }
 
 func (p *PytestRunner) GetRunCommand(cases []*openapi.ObjectFunctionWithSignature) string {
-	// py.test tests_directory/foo.py tests_directory/bar.py -k 'test_001 or test_some_other_test'
+	// pytest test/test_api.py::test_analyse test/test_video.py::test_read_from_file
+	// https://docs.pytest.org/en/7.1.x/how-to/usage.html#specifying-which-tests-to-run
 	if len(cases) == 0 {
 		return "--ignore-glob=\"*\""
 	}
 
 	execCmdList := make([]string, 0, len(cases))
 	for _, each := range cases {
-		execCmdList = append(execCmdList, fmt.Sprintf("%s", each.GetName()))
+		// use relative path
+		relPath := strings.TrimPrefix(each.GetPath(), p.config.SrcDir)
+		eachStr := fmt.Sprintf("%s::%s", relPath, each.GetName())
+		execCmdList = append(execCmdList, eachStr)
 	}
-	caseRegex := strings.Join(execCmdList, " and ")
-	finalCaseStr := fmt.Sprintf("-k=\"%s\"", caseRegex)
-	return finalCaseStr
+	finalStr := strings.Join(execCmdList, " ")
+	return finalStr
 }
 
 func NewPytestRunner(conf *object.SharedConfig) (Runner, error) {
